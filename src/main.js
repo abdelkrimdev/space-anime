aac4985dca0fc5f55ae8835ec53adeb091947003
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2019-07-13 18:48:31 +0100

    read and build handlebars template for posts

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
-// mustache posts/011-working!!!-tv.json templates/post.mustache > output.html
-// import mustache from "mustache"
+import fs from 'fs'
+import handlebars from 'handlebars'
+import path from 'path'
+
+const directoryPath = (directoryName) => path.resolve('src', directoryName)
+const filePath = (directoryPath, fileName) => path.join(directoryPath, fileName)
+
+const buildTemplate = (templatePath) => {
+  fs.readFile(templatePath, 'utf8', (error, content) => {
+    if (error) {
+      console.log(`An error occurred while reading ${templatePath}, ${error}`)
+    }
+    if (content) {
+      return handlebars.compile(content)
+    }
+  })
+}
+
+// Build templates
+const templatesDir = directoryPath('templates')
+
+const postTemplatePath = filePath(templatesDir, 'post.handlebars')
+const postTemplate = buildTemplate(postTemplatePath)
+
+// Generate HTML for blog posts
+const postsDir = directoryPath('posts')
